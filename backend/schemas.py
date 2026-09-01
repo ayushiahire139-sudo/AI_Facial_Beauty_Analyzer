47c3983fbeea3111f517eb588784e695b56c452b
ayushi <ayushiahire139@gmail.com>
2026-09-01 17:26:34 +0530

    Fix email validation dependency in requirements.txt and schemas.py for seamless cloud execution

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,14 +1,12 @@
-from pydantic import BaseModel, EmailStr
-
+from pydantic import BaseModel
 
 class UserCreate(BaseModel):
     fullname: str
-    email: EmailStr
+    email: str
     password: str
 
-
 class UserLogin(BaseModel):
-    email: EmailStr
+    email: str
     password: str
 
 class ImageResponse(BaseModel):
